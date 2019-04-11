7f81b6a84dc40825b3096633055020899418f6f1
kludDesign <vincentgillibert@gmail.com>
2019-04-11 20:00:29 +0200

    update with comments

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -4,8 +4,6 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-
-
 def get_ville
 
   ville_name_array = []
@@ -22,28 +20,27 @@ def get_email (ville_names)
 
   ville_email_array = []
 
+  # Loop on each cities in the array to get the email
   for n in 0...ville_names.length
 
       page_url_ville = "https://www.annuaire-des-mairies.com/95/#{ville_names[n]}.html"
 
       ville_page = Nokogiri::HTML(open(page_url_ville))
 
-      # begin
-      ville_email_array << ville_page.xpath("//html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]/text()").to_s
-      # rescue => e
-      #   ville_email_array << "lol" #résultat que tu veux voir en lieu et place d'une erreur terminal
-      # end
+      # If bug to get any email
+      begin
+        # Put each email in an array "ville_email_array"
+        ville_email_array << ville_page.xpath("//html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]/text()").to_s
+
+      rescue => e
+
+        ville_email_array << " " #résultat que tu veux voir en lieu et place d'une erreur terminal
+
+      end
   end
 
   return ville_email_array
 end
 
+# Merge ville array with email array
 puts email_ville_result = Hash[get_ville.zip(get_email(get_ville))]
-
-# # From the website, get an array of the currencies price and convert it to string
-# currency_value_array = page.xpath("//tr/td/a[contains(@class, 'price')]/text()").map {|x| x.to_s }
-#
-# # Convert the 2 arrays into hash
-# currency_result = Hash[currency_name_array.zip(currency_value_array)]
-#
-# puts currency_result
